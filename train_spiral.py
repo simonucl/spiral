61dcbe55fe632c6cb4bcdf658eb9416bf09b718f
simon <simon011130@gmail.com>
2025-09-06 11:25:18 -0700

    set filter draw to false default

--- train_spiral.py
+++ train_spiral.py
@@ -76,7 +76,7 @@ class SelfPlayArgs(PPOArgs):
     filter_zero_adv: bool = (
         True  # Make gradient less noisy by filtering zero-gradient trajectories
     )
-    filter_draw: bool = True  # Filter out draw games
+    filter_draw: bool = False  # Filter out draw games
     use_role_baseline: bool = True  # Use role baseline for reward shaping
     role_baseline_ema_gamma: float = 0.95
 
